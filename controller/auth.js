dbbef0ba2fcf9068aa8528f4d2be2ccc61a04e50
jaehunju1996 <jjdong9108@gmail.com>
2023-02-23 13:23:20 +0900

    Jaehun
    feat : 로그아웃 구현완료

--- controller/auth.js
+++ controller/auth.js
@@ -1,15 +1,19 @@
-import AuthService from "../services/auth.js";
+import AuthService from '../services/auth.js';
 
 class AuthController {
-  authService = new AuthService();
+    authService = new AuthService();
 
-  signup = async (req, res) => {
-    return await this.authService.createUserService(req, res);
-  };
+    signup = async (req, res) => {
+        return await this.authService.createUserService(req, res);
+    };
 
-  login = async (req, res) => {
-    return await this.authService.loginUserService(req, res);
-  };
+    login = async (req, res) => {
+        return await this.authService.loginUserService(req, res);
+    };
+
+    logout = async (req, res) => {
+        return await this.authService.logoutUserService(req, res);
+    };
 }
 
 export default AuthController;
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
-import express from "express";
-import AuthController from "../controller/auth.js";
-import * as validate from "../middleware/validator.js";
+import express from 'express';
+import AuthController from '../controller/auth.js';
+import * as validate from '../middleware/validator.js';
 const router = express.Router();
 const authController = new AuthController();
 
-router.post("/signup", validate.validate, authController.signup);
+router.post('/signup', validate.validate, authController.signup);
 
-router.post("/login", authController.login);
+router.post('/login', authController.login);
+
+router.post('/logout', authController.logout);
 
 export default router;
