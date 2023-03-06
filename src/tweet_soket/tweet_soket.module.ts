0447e5e15ec113251a5bf404bbd5ffa1a130945c
한창윤 <hansyooni11@gmail.com>
2023-03-06 10:23:38 +0900

    HanChangYun
    웹소켓 기본, 파일올리기 수정

--- src/tweet_soket/tweet_soket.module.ts
+++ src/tweet_soket/tweet_soket.module.ts
@@ -1,7 +1,7 @@
-// import { Module } from '@nestjs/common';
-// import { TweetSocketGateway } from './tweet_soket.gateway';
+import { Module } from '@nestjs/common';
+import { TweetSocketGateway } from './tweet_soket.gateway';
 
-// @Module({
-//   providers: [TweetSocketGateway],
-// })
-// export class TweetSoketModule {}
+@Module({
+  providers: [TweetSocketGateway],
+})
+export class TweetSoketModule {}
