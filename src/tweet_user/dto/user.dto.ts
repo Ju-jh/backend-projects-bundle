738db8433cc3fa1d8a9fafd545095e6a321cf986
한창윤 <hansyooni11@gmail.com>
2023-03-07 17:28:41 +0900

    HanChangYun
    유저 다시 수정

--- src/tweet_user/dto/user.dto.ts
+++ src/tweet_user/dto/user.dto.ts
@@ -1,14 +1,18 @@
-import { IsEmail, IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateUserDto {
-  @IsEmail()
-  user_email: string;
+  @IsOptional()
+  @IsNumber()
+  id?: number;
+
+  @IsString()
+  user_email?: string;
 
   @IsString()
   user_name: string;
 
   @IsString()
-  user_password: string;
+  user_password?: string;
 
   @IsString()
   user_birthday?: string;
