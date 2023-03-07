acf50b946e12c061925eca23b6dd0c9ce6595e8c
LKW9 <92284361+LKW9@users.noreply.github.com>
2023-03-07 18:09:22 +0900

    env 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,16 +11,20 @@ import { TweetCommentModule } from './tweet_comment/tweet_comment.module';
 import { Tweet } from './tweet_post/entities/tweet.entity';
 import { TweetComment } from './tweet_comment/entities/tweet_comment.entity';
 import { Like } from './tweet_post/entities/tweetLike.entity';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'Qwer!234',
-      database: 'tweet',
+      host: process.env.HOST,
+      port: parseInt(process.env.PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_DATABASE,
       entities: [User, Profile, Tweet, TweetComment, Like],
       synchronize: true,
       autoLoadEntities: true,
