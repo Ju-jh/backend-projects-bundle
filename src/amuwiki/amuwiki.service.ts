b0d131eb33a06cf35c2d1f69d32b846d08cb9db0
한창윤 <hansyooni11@gmail.com>
2023-03-10 23:26:57 +0900

    repairing amuwiki
    repairing

--- src/amuwiki/amuwiki.service.ts
+++ src/amuwiki/amuwiki.service.ts
@@ -1,42 +1,31 @@
-// import { Injectable } from '@nestjs/common';
-// import { ElasticsearchService } from '@nestjs/elasticsearch';
-// import { InjectModel } from '@nestjs/mongoose';
-// import { Model } from 'mongoose';
-// import { SearchAmuwikiDto } from './dto/searchAmuwiki.dto';
-// import { Amuwiki } from './schema/amuwiki.schema';
+import { Injectable } from '@nestjs/common';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Amuwiki } from './schema/amuwiki.schema';
 
-// @Injectable()
-// export class AmuwikiService {
-//   constructor(
-//     private readonly elasticsearchService: ElasticsearchService,
-//     @InjectModel('Amuwiki') private AmuwikiModel: Model<Amuwiki>,
-//   ) {}
+@Injectable()
+export class AmuwikiService {
+  constructor(
+    private readonly elasticsearchService: ElasticsearchService,
+    @InjectModel('Amuwiki') private AmuwikiModel: Model<Amuwiki>,
+  ) {}
 
-//   async search(query: string) {
-//     const body = await this.elasticsearchService.search({
-//       index: 'Amuwikis',
-//       body: {
-//         query: {
-//           match: {
-//             query: query,
-//           },
-//         },
-//       },
-//     });
-//     console.log(body);
+  async search(query: string): Promise<any[]> {
+    const result = await this.elasticsearchService.search({
+      index: 'amuwikis', // Elasticsearch에서 MongoDB 데이터를 검색할 인덱스 이름
+      body: {
+        query: {
+          multi_match: {
+            query: 'search keyword',
+            fields: ['title', 'text'],
+          },
+        },
+      },
+    });
+    console.log('@@@@@@@@@@@@@@@', result);
+    console.log('###############', result.hits.hits);
 
-//     const hits = body.hits.hits;
-//     console.log(hits);
-
-//     const amuwikis = hits.map((hit) => {
-//       return {
-//         id: hit._id,
-//         namespace: hit._source.namespace,
-//         title: hit.title,
-//         text: hit.text,
-//       };
-//     });
-
-//     return Amuwiki;
-//   }
-// }
+    return result.hits.hits;
+  }
+}
