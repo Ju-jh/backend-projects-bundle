ec6d9393daf56868e9896c8dd37d378a8217bee6
jaehunju1996 <jjdong9108@gmail.com>
2023-03-12 20:56:39 +0900

    feat: Rebuilding Login part
    I installed nothing
    
    I rebuilt login part
    
    thank you.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,6 +8,16 @@ export class AuthController {
   @Post('login')
   async login(@Body() body, @Response({ passthrough: true }) res) {
     const isUser = await this.authService.findByEmail(body.email);
+    if (!isUser) {
+      return { message: '이메일 혹은 패스워드를 찾을 수 없습니다.' };
+    }
+    const isPassword = await this.authService.validateUser(
+      body.email,
+      body.password,
+    );
+    if (isPassword === false) {
+      return { message: '이메일 혹은 패스워드를 찾을 수 없습니다.' };
+    }
     const isLogin = await this.authService.login(isUser);
     res.cookie('Authentication', `Bearer ${isLogin}`);
     return { message: 'AMUWIKI에 오신걸 환영합니다.' };
