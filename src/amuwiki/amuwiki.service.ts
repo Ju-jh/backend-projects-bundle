e5c27c7becc059db4f4b895ae784a6656ae26b81
jaehunju1996 <jjdong9108@gmail.com>
2023-03-12 19:37:35 +0900

    feat:  Add method of sending verifying code to email to Signup part
    I installed "nodemailer": "^6.9.1"
    
    please check.
    
    thank you.

--- src/amuwiki/amuwiki.service.ts
+++ src/amuwiki/amuwiki.service.ts
@@ -1,31 +1,31 @@
-import { Injectable } from '@nestjs/common';
-import { ElasticsearchService } from '@nestjs/elasticsearch';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Amuwiki } from './schema/amuwiki.schema';
+// import { Injectable } from '@nestjs/common';
+// import { ElasticsearchService } from '@nestjs/elasticsearch';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Model } from 'mongoose';
+// import { Amuwiki } from './schema/amuwiki.schema';
 
-@Injectable()
-export class AmuwikiService {
-  constructor(
-    private readonly elasticsearchService: ElasticsearchService,
-    @InjectModel('Amuwiki') private AmuwikiModel: Model<Amuwiki>,
-  ) {}
+// @Injectable()
+// export class AmuwikiService {
+//   constructor(
+//     private readonly elasticsearchService: ElasticsearchService,
+//     @InjectModel('Amuwiki') private AmuwikiModel: Model<Amuwiki>,
+//   ) {}
 
-  async search(query: string): Promise<any[]> {
-    const result = await this.elasticsearchService.search({
-      index: 'amuwikis', // Elasticsearch에서 MongoDB 데이터를 검색할 인덱스 이름
-      body: {
-        query: {
-          multi_match: {
-            query: 'search keyword',
-            fields: ['title', 'text'],
-          },
-        },
-      },
-    });
-    console.log('@@@@@@@@@@@@@@@', result);
-    console.log('###############', result.hits.hits);
+//   async search(query: string): Promise<any[]> {
+//     const result = await this.elasticsearchService.search({
+//       index: 'amuwikis', // Elasticsearch에서 MongoDB 데이터를 검색할 인덱스 이름
+//       body: {
+//         query: {
+//           multi_match: {
+//             query: 'search keyword',
+//             fields: ['title', 'text'],
+//           },
+//         },
+//       },
+//     });
+//     console.log('@@@@@@@@@@@@@@@', result);
+//     console.log('###############', result.hits.hits);
 
-    return result.hits.hits;
-  }
-}
+//     return result.hits.hits;
+//   }
+// }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,29 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { CreateUserDto } from './dto/user.dto';
+import { VerifyEmailDto } from './dto/verifyEmail.dto';
 import { UserService } from './user.service';
 
-@Controller('users')
+@Controller('signup')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    this.userService.create(createUserDto);
+  @Post('email')
+  async verifyEmail(@Body() verifyEmailDto: VerifyEmailDto) {
+    const isEmailValid = this.userService.validateEmail(verifyEmailDto.email);
+    if (!isEmailValid) {
+      return { message: '올바른 이메일 형식이 아닙니다.' };
+    }
+    const isEmailExist = await this.userService.isEmailExist(
+      verifyEmailDto.email,
+    );
+    if (isEmailExist) {
+      return { message: '이미 존재하는 이메일입니다.' };
+    }
+    const emailVerificationCode = this.userService.sendEmailVerificationCode(
+      verifyEmailDto.email,
+    );
+    emailVerificationCode;
+    return {
+      message: '이메일 인증 코드가 발송되었습니다. 인증 코드를 입력하세요.',
+    };
   }
 }
