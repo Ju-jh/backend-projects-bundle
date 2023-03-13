43a2bd7d580b487c4a2360d7b950493e86fa5023
LKW9 <92284361+LKW9@users.noreply.github.com>
2023-03-13 16:30:07 +0900

    feat: Building Profile edit part
    I installed nothing

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Headers, Post } from '@nestjs/common';
+import { Put } from '@nestjs/common/decorators';
 import { AuthService } from 'src/auth/auth.service';
 import { CreateProfileDto } from './dto/createProfile.dto';
+import { EditProfileDto } from './dto/editProfile.dto';
 import { ProfileService } from './profile.service';
 @Controller('profile')
 export class ProfileController {
@@ -30,5 +32,16 @@ export class ProfileController {
   async getDetailProfile(@Headers('cookie') cookie) {
     const info = await this.authService.parseToken(cookie);
     return await this.profileService.getEditProfile(info);
+  }
+
+  @Put('/edit')
+  async editProfile(
+    @Headers('cookie') cookie,
+    @Body() updateData: EditProfileDto,
+  ) {
+    const info = await this.authService.parseToken(cookie);
+    const email = Object.values(info)[0];
+    await this.profileService.editProfile(email, updateData);
+    return { message: '프로필 정보가 변경되었습니다.' };
   }
 }
