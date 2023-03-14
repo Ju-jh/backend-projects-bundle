d8c9dce9a267c596c8acc67dd926a371df62829b
Jayden Ju (주재훈) <121030294+jaehunju1996@users.noreply.github.com>
2023-03-14 18:21:46 +0900

    feat: Building Membership withdrawl part
    I installed nothing
    please check.
    
    I finished all my building part
    and I have checked there is no problem with using our application.
    
    So now after close this issue I'll do refactoring code which part is user file and auth file.
    
    thank you.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,11 @@ import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { User, UserDocument } from 'src/user/schemas/user.schema';
 import { CreateUserDto } from 'src/user/dto/user.dto';
+import { DeleteUserDto } from './dto/deleteuser.dto';
+import {
+  VerifiedEmail,
+  VerifiedEmailDocument,
+} from 'src/user/schemas/VerifiedEmail.schema';
 
 @Injectable()
 export class AuthService {
@@ -12,6 +17,8 @@ export class AuthService {
     private jwtService: JwtService,
     @InjectModel(User.name)
     private userModel: Model<UserDocument>,
+    @InjectModel(VerifiedEmail.name)
+    private verifiedEmail: Model<VerifiedEmailDocument>,
   ) {}
 
   async validateUser(email: string, password: string): Promise<boolean> {
@@ -41,5 +48,33 @@ export class AuthService {
   async parseToken(cookie: string): Promise<any> {
     const splitedToken = cookie.split('%20')[1];
     return this.decodeToken(splitedToken);
+  }
+
+  async deleteUser(
+    email: string,
+    deleteUserDto: DeleteUserDto,
+  ): Promise<{ message: string }> {
+    const user = await this.findByEmail(email);
+    if (!user) {
+      return { message: '사용자를 찾을 수 없습니다.' };
+    }
+    const isPasswordValid = await bcrypt.compare(
+      deleteUserDto.password,
+      user.password,
+    );
+    if (!isPasswordValid) {
+      return { message: '비밀번호가 올바르지 않습니다.' };
+    }
+    const deletedUser = await this.userModel.findOneAndDelete({ email });
+    if (!deletedUser) {
+      return { message: '사용자 삭제에 실패했습니다.' };
+    }
+    const deletedvalidateEmail = await this.verifiedEmail.findOneAndDelete({
+      email,
+    });
+    if (!deletedvalidateEmail) {
+      return { message: '이메일 인증 정보 삭제에 실패했습니다.' };
+    }
+    return { message: '사용자 삭제에 성공했습니다.' };
   }
 }
