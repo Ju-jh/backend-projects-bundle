f202ed2c60ea9d6dad033b7ed3f367b59d7cafa6
Hyunsoo <kaist217@naver.com>
2023-03-15 22:47:05 +0900

    feat: Refactoring post file
    I installed nothing.
    
    Thank you.

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,44 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Put, Delete, Res } from '@nestjs/common';
 import { Body, Headers, Post } from '@nestjs/common/decorators';
-import {
-  Delete,
-  Put,
-} from '@nestjs/common/decorators/http/request-mapping.decorator';
-import { AuthService } from 'src/auth/auth.service';
 import { CreatePostDto } from './dto/createPost.dto';
 import { EditPostDto } from './dto/editPost.dto';
 import { PostService } from './post.service';
+import { FastifyReply } from 'fastify';
 
 @Controller('post')
 export class PostController {
-  constructor(
-    private postService: PostService,
-    private authService: AuthService,
-  ) {}
+  constructor(private postService: PostService) {}
 
   @Post()
-  async createPost(@Headers('cookie') cookie, @Body() postData: CreatePostDto) {
-    const info = await this.authService.parseToken(cookie);
-    const email = Object.values(info)[0];
-    console.log('#############', info);
-    console.log('@@@@@@@@', typeof email);
-    this.postService.createPost(postData, email);
-    return { message: '게시글이 생성되었습니다.' };
+  async createPost(
+    @Headers('cookie') cookie: string,
+    @Body() createPostDto: CreatePostDto,
+    @Res() res: FastifyReply,
+  ) {
+    const email = await this.postService.detoken(cookie);
+    await this.postService.createPost(createPostDto, email);
+    res.send({ message: '게시글이 생성되었습니다.' });
   }
 
   @Put()
-  async editPost(@Headers('cookie') cookie, @Body() updateData: EditPostDto) {
-    const info = await this.authService.parseToken(cookie);
-    const email = Object.values(info)[0];
-    this.postService.editPost(email, updateData);
-    return { message: '게시글이 수정되었습니다.' };
+  async editPost(
+    @Headers('cookie') cookie,
+    @Body() editPostDto: EditPostDto,
+    @Res() res: FastifyReply,
+  ) {
+    const email = await this.postService.detoken(cookie);
+    this.postService.editPost(email, editPostDto);
+    res.send({ message: '게시글이 수정되었습니다.' });
   }
 
   @Delete()
-  async deletePost(@Headers('cookie') cookie) {
-    const info = await this.authService.parseToken(cookie);
-    const email = Object.values(info)[0];
+  async deletePost(@Headers('cookie') cookie, @Res() res: FastifyReply) {
+    const email = await this.postService.detoken(cookie);
     this.postService.deletePost(email);
-    return { message: '게시글이 삭제되었습니다.' };
+    res.send({ message: '게시글이 삭제되었습니다.' });
   }
 }
