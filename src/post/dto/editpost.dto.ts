6caf3e15c33ece63f15a4b2903d7f770cd82fa75
Jayden Ju (주재훈) <121030294+jaehunju1996@users.noreply.github.com>
2023-03-21 00:18:08 +0900

    feat: Refactoring for add objectId
    We added objectId for post
    
    and we added profile url too.

--- src/post/dto/editpost.dto.ts
+++ src/post/dto/editpost.dto.ts
@@ -3,6 +3,10 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class EditPostDto {
   @IsNotEmpty()
   @IsString()
+  _id: string;
+
+  @IsNotEmpty()
+  @IsString()
   title: string;
 
   @IsNotEmpty()
