695f1271d6830ccd3291ef98fe49e8dd0678c792
한창윤 <hansyooni11@gmail.com>
2023-03-22 09:49:21 +0900

    Refactoring Amuwiki Search
    Refactoring Amuwiki Search

--- src/amuwiki/amuwiki.controller.ts
+++ src/amuwiki/amuwiki.controller.ts
@@ -1,5 +1,5 @@
-import { FastifyRequest, FastifyReply } from 'fastify';
-import { Controller, Get, Query, Req, Res } from '@nestjs/common';
+import { FastifyReply } from 'fastify';
+import { Controller, Get, Query, Res } from '@nestjs/common';
 import { AmuwikiService } from './amuwiki.service';
 
 @Controller('amuwiki')
@@ -7,11 +7,7 @@ export class AmuwikiController {
   constructor(private readonly amuwikiService: AmuwikiService) {}
 
   @Get('/search')
-  async search(
-    @Query('query') query: string,
-    @Req() req: FastifyRequest,
-    @Res() res: FastifyReply,
-  ) {
+  async search(@Query('query') query: string, @Res() res: FastifyReply) {
     const result = await this.amuwikiService.search(query);
     res.send(result);
   }
